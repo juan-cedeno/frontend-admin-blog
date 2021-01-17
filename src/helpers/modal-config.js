597bce27b45e52fa07e5cee650495696830204e3
Juan Cedeño <juancc0315@gmail.com>
2021-01-18 02:24:35 +0800

    second commit

--- src/helpers/modal-config.js
+++ src/helpers/modal-config.js
@@ -1,6 +1,6 @@
 export const customStyles = {
     content : {
-      top                   : '45%',
+      top                   : '50%',
       left                  : '50%',
       right                 : 'auto',
       bottom                : 'auto',
